79284cca98f29858611deeb9e719834f5179b4ff
darjwx <darjwx@gmail.com>
2017-05-14 20:33:18 +0200

    BuildScript: add profiles option
    *Two pre-configured profiles to speed up the process of launching a build if you dont want anything specific.

--- BuildScript.sh
+++ BuildScript.sh
@@ -2,27 +2,32 @@
 
 mainMenu (){
 
-	while :
-	do
-		echo "1. Repo sync"
-		echo "2. Compile"
-		echo "3. Exit"
+    while :
+    do
+        echo "1. Repo sync"
+        echo "2. Compile"
+        echo "3. Userdebug personal build"
+        echo "4. Eng experimental build"
+        echo "5. Exit"
 
-	    read -r -p "Choose an option: " option
+        read -r -p "Choose an option: " option
 
-	    case "$option" in
-		    "1") repoSync
+        case "$option" in
+            "1") repoSync
             ;;
             "2") compile
             ;;
-            "3") exit
+            "3") profile1
+            ;;
+            "4") profile2
+            ;;
+            "5") exit
 
         esac
 
         clear
 
     done
-
 
 }
 
@@ -53,6 +58,30 @@ compile (){
     read -r -p "BUILD_VARIANT=" variant
 
     lunch carbon_bacon-$variant
+
+    make carbon -j8
+
+}
+
+profile1 (){
+
+    source build/envsetup.sh
+
+    export CARBON_BUILDTYPE=Personal
+
+    lunch carbon_bacon-userdebug
+
+    make carbon -j8
+
+}
+
+profile2 (){
+
+    source build/envsetup.sh
+
+    export CARBON_BUILDTYPE=Experimental
+
+    lunch carbon_bacon-eng
 
     make carbon -j8
 
