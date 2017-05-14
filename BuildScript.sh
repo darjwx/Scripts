d064ecafcb19eee4a87bebdabff5830e65f615df
darjwx <darjwx@gmail.com>
2017-05-14 18:09:30 +0200

    BuildScript: add the option to repo sync

--- BuildScript.sh
+++ BuildScript.sh
@@ -1,11 +1,23 @@
+read -r -p "Repo sync (y/n): " repoSync
+
+if [ "$repoSync" = "y" ]; then
+
+	repo sync --force-sync
+
+else
+
+	echo "Repo syncing disabled"
+
+fi
+
 source build/envsetup.sh
 
 read -r -p "CARBON_BUILDTYPE=" crtype
 
 export CARBON_BUILDTYPE=$crtype
 
-read -r -p "Eng/Userdebug" btype
+read -r -p "BUILD_VARIANT=" variant
 
-lunch carbon_bacon-$btype
+lunch carbon_bacon-$variant
 
 make carbon -j8
