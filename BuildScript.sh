a035d95b2072dc85c5a50f5f483b58f26a54bde4
darjwx <darjwx@gmail.com>
2017-05-16 17:26:56 +0200

    scripts: add GPL v3

--- BuildScript.sh
+++ BuildScript.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+#
+# Copyright (C) 2017, Dario Jimenez "darjwx" <darjwx@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 function mainMenu() {
 
@@ -11,8 +26,8 @@ function mainMenu() {
 
 	    read -r -p "Choose an option: " option;
 
-	    case ${option} in
-		    1) repoSync;
+        case ${option} in
+            1) repoSync;
             ;;
             2) compile;
             ;;
